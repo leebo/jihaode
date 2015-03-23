de332909f36f8fb765b1badffeb01f434e955ad2
leebo <me@bobo.im>
2015-03-23 23:59:55 +0800

    增加factory_girl前缀修改

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,12 +1,3 @@
-Spec.configure do |config|
-  # additional factory_girl configuration
-
-  config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
-    end
-  end
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
 end
