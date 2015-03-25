9750feb220b1fc50a07f7d7c2b08189038dd9e33
leebo <me@bobo.im>
2015-03-26 03:07:39 +0800

    同上一次修改

---
+++ app/jobs/one_job.rb
@@ -0,0 +1,10 @@
+class OneJob
+  include Sidekiq::Worker
+
+
+  def perform
+    # do stuff ...
+    #   end
+    #   end
+  end
+end
--- spec/jobs/guests_cleanup_job_spec.rb
+++ spec/jobs/guests_cleanup_job_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe GuestsCleanupJob, type: :job do
+RSpec.describe OneJob, type: :job do
   it 'can preform' do
-    GuestsCleanupJob.set(wait_until: 1.minute.from_now).perform_later
-    GuestsCleanupJob.perform_later
+    OneJob.perform_in(10.second)
   end
+
 end
